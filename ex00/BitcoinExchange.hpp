9541fb849ba3f98c5ac39b1ac442e68199c7c6fd
salee2 <hleesa>
2023-08-01 17:09:11 +0900

    temp

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -2,8 +2,19 @@
 #ifndef CPP_HLEESA_BITCOINEXCHANGE_HPP
 #define CPP_HLEESA_BITCOINEXCHANGE_HPP
 
+#include <queue>
+#include <string>
+#include <utility>
+
+typedef std::pair<std::string, double> dateValuePair;
 
 class BitcoinExchange {
+
+private:
+    std::queue <dateValuePair> inputData;
+
+public:
+
 
 };
 
